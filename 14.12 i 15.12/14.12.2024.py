25ee4e9757d51199588a5b99cdc45998906c0222
Roksana <r.szczypek00@gmail.com>
2024-12-14 09:02:15 +0100

    new files 14.12

---
+++ 14.12 i 15.12/14.12.2024.py
@@ -0,0 +1,8 @@
+def f(liczba1 = 2, liczba2 = 3, operator = '/'):
+    wynik = "nieznany operator"
+if operator == "+":
+    wynik = liczba1 + liczba2
+elif operator == "-":
+    wynik = liczba1 - liczba2
+
+print()
--- practice/zad_sama.py
+++ practice/zad_sama.py
@@ -16,14 +16,48 @@
 
 # zad 1.1.2
 
-original = ["I", "am", "learning", "hacking", "in"]
+# original = ["I", "am", "learning", "hacking", "in"]
 
-modified = original[:]
-modified[3] = "lists"
-modified.append("Python")
+# modified = original[:]
+# modified[3] = "lists"
+# modified.append("Python")
 
-assert original == ["I", "am", "learning", "hacking", "in"]
-assert modified == ["I", "am", "learning", "lists", "in", "Python"]
+# assert original == ["I", "am", "learning", "hacking", "in"]
+# assert modified == ["I", "am", "learning", "lists", "in", "Python"]
 
-print(modified)
-print(original)
+# print(modified)
+# print(original)
+
+# zadanie 1.1.3
+
+# list1 = [6, 12, 5]
+# list2 = [6.2, 0, 14, 1]
+# list3 = [0.9]
+
+# combined = list1 + list2 + list3
+
+# combined.sort()
+
+# combined.reverse()
+
+# my_list = combined
+# print(my_list)
+
+# zadanie 1.2.1
+
+first_name = "John"
+last_name = "Doe"
+favorite_hobby = "Python"
+sports_hobby = "gym"
+age = 82
+
+name = {"John", "Doe"}
+age = {"age": 82}
+hobbies = {"favorite_hobby": "Python", "sports_hobby": "gym"}
+
+my_dict = {}
+my_dict.update(name)
+my_dict.update(age)
+my_dict.update(hobbies)
+
+print(my_dict)
